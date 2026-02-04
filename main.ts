9a5f3209c660f90aaf1dae172772d37ed3f50695
katherine <katherine.malleck@stu.ocsb.ca>
2026-02-04 13:21:50 -0500

    I filled in the comment

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Katherine
+ * Created on: Feb 2026
+ * This program will print Hello, World! 
 */
 
 basic.showString('Hello, World!')
